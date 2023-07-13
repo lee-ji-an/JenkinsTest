2ece7a58dd0a9a98a342bcc8a4bc5d9723af3655
lee-ji-an <leeja0407@naver.com>
2023-07-13 16:36:46 +0900

    Update HelloController message

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String index(){
-        return "Hello world";
+        return "Hello world !!!";
     }
 }
